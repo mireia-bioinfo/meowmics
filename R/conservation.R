063115771f2188eff70081e294897b89f9a816a3
mireia-bioinfo <mireiarr9@gmail.com>
2022-02-18 14:22:54 +0100

    Added fixed number of bins mode to conservation scores

--- R/conservation.R
+++ R/conservation.R
@@ -5,9 +5,12 @@
 #' @param id_col Number or name of `mcol` that contains unique identifier for each peak. Default: 1.
 #' @param cons_score Annotation to use for getting the conservation scores. See \code{\link[GenomicScores]{gscores}}
 #' for details.
-#' @param bin_width Number of base pairs for binning the peak and obtaining the conservation scores. Default: 20
+#' @param bin_width Number of base pairs for binning the peak and obtaining the conservation scores.
+#' Ignored if the Fixed Number of Bins mode is activated (see Details). Default: 20
 #' @param window_width Number of base pairs of a window centered in the center of the peak where to focus the
-#' conservation analysis. Default = 2000
+#' conservation analysis. Ignored if the Fixed Number of Bins mode is activated (see Details). Default = 2000
+#' @param n_bins Integer width the number of bins each region should be divided into. Giving it a value activates
+#' the Fixed Number of Bins mode (see Details).
 #' @param merge_fun Function for summarizing scores in the same region.
 #' @param random_control Logical indicating whether to perform the same analysis using a randomized set of peaks
 #' as a control. Default = TRUE
@@ -17,6 +20,10 @@
 #' the original set of peaks. See \code{\link[regioneR]{randomizeRegions}} for details.
 #' @param summarise Logaical indicating whether to summarise the results, computing the mean of all peaks at a specific
 #' position. Default: TRUE.
+#' @details This function has two modes to calculate conservation scores in a set of regions: fixed window sizes (default)
+#' or fixed number of bins.
+#' - **Fixed window sizes**. Regions are resized to be `window_width` and are then divided into `bin_width` bp bins.
+#' - **Fixed number of bins**. Regions are divided into `n_bins`, idependently of their size.
 #' @return It returns a \code{data.frame} containing the summarized phastCons conservation scores for the file
 #' and randomized control (if \code{random_control = TRUE}) at each relative position from the peak center.
 #' The summarization is performed by computing the mean for all peaks in that specific position. Standard deviation
@@ -27,22 +34,32 @@ get_conservation_scores <- function(peak_file,
                                     cons_score=phastCons7way.UCSC.hg38::phastCons7way.UCSC.hg38,
                                     bin_width=20,
                                     window_width=2e3,
+                                    n_bins=NULL,
                                     merge_fun="mean",
                                     random_control=TRUE,
                                     genome="hg38",
                                     mask=NULL,
                                     per.chromosome=FALSE,
                                     summarise=TRUE) {
+  if (is.null(n_bins)) {
+    ## If same size windows + bins
+    peaks <- regioneR::toGRanges(peak_file)
+    peaks <- GenomicRanges::resize(peaks, width=window_width, fix="center")
+    peaks_bin <- GenomicRanges::tile(peaks, width=bin_width)
+    pos <- seq(-window_width/2, window_width/2-1, by=bin_width)
 
-  peaks <- regioneR::toGRanges(peak_file)
-  peaks <- GenomicRanges::resize(peaks, width=window_width, fix="center")
-  peaks_bin <- GenomicRanges::tile(peaks, width=bin_width)
-  pos <- seq(-window_width/2, window_width/2-1, by=bin_width)
+    peaks_unl <- unlist(peaks_bin)
+    peaks_unl$id <- rep(paste0("sample_peak_", 1:length(peaks)), each=length(pos))
+    peaks_unl$pos <- rep(pos, length(peaks_bin))
+  } else {
+    ## If fixed number of bins
+    peaks_tile <- GenomicRanges::tile(peaks, n=n_bins)
+    peaks_unl <- unlist(peaks_tile)
+    peaks_unl$id <- rep(paste0("sample_peak_", 1:length(peaks)), each=n_bins)
+    peaks_unl$pos <- rep(1:100, length(peaks_tile))
+  }
 
-  peaks_unl <- unlist(peaks_bin)
-  peaks_unl$id <- rep(paste0("sample_peak_", 1:length(peaks)), each=length(pos))
-  peaks_unl$pos <- rep(pos, length(peaks_bin))
-
+  ## Get scores
   scores <- GenomicScores::gscores(cons_score,
                                    peaks_unl,
                                    summaryFun=merge_fun)
@@ -53,11 +70,22 @@ get_conservation_scores <- function(peak_file,
                                        mask=mask,
                                        per.chromosome=per.chromosome)
     rndm <- rndm[order(rndm),]
-    rndm_bin <- GenomicRanges::tile(rndm, width=bin_width)
-    rndm_unl <- unlist(rndm_bin)
-    rndm_unl$id <- rep(paste0("random_peak_", 1:length(rndm)), each=length(pos))
-    rndm_unl$pos <- rep(pos, length(rndm_bin))
 
+    if (is.null(n_bins)) {
+      ## If same size windows + bins
+      rndm_bin <- GenomicRanges::tile(rndm, width=bin_width)
+      rndm_unl <- unlist(rndm_bin)
+      rndm_unl$id <- rep(paste0("random_peak_", 1:length(rndm)), each=length(pos))
+      rndm_unl$pos <- rep(pos, length(rndm_bin))
+    } else {
+      ## If fixed number of bins
+      rndm_tile <- GenomicRanges::tile(rndm, n=n_bins)
+      rndm_unl <- unlist(rndm_tile)
+      rndm_unl$id <- rep(paste0("sample_peak_", 1:length(rndm)), each=n_bins)
+      rndm_unl$pos <- rep(1:100, length(rndm_tile))
+    }
+
+    ## Get scores
     scores_rndm <- GenomicScores::gscores(cons_score,
                                           rndm_unl)
 
